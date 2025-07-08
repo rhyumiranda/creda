60edc029b1a91ee00636081589fd49c12acb8634
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-07-09 06:35:15 +0800

    chore: create a pocketbase hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import Pocketbase from "pocketbase";
 
-
+export const pb = new Pocketbase("http://127.0.0.1:8090");
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
