662c42ff5f435dbb2ea967445620b0e93d01c771
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-07-09 23:52:20 +0800

    feat: update

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import Pocketbase from "pocketbase";
 
-export const pb = new Pocketbase("http://127.0.0.1:8090");
+export const pb = new Pocketbase(process.env.NEXT_PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090");
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
